af6c069506956cfdd8c382ed154851db3a937db5
sumitrawal658 <133184340+sumitrawal658@users.noreply.github.com>
2025-01-07 12:38:53 +0700

    Adding Readme.md

--- HighWorkflowSystemDesign/OrderService/src/main/java/org/highload/config/WebClientConfig.java
+++ HighWorkflowSystemDesign/OrderService/src/main/java/org/highload/config/WebClientConfig.java
@@ -1,5 +1,8 @@
 package org.highload.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class WebClientConfig {
 
